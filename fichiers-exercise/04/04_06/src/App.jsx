9ce7f5de92148126d6e8097d1f05477c87b099bc
Sandy <sandy@MacBookPro.lan>
2025-07-01 16:13:07 +0200

    04_06_d

---
+++ fichiers-exercise/04/04_06/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { createContext, useContext, useState } from 'react';
+const style = {
+  backgroundColor: isDark ? '#222' : '#f9f9f9',
+  color: isDark ? '#fff' : '#000',
+  height: '100vh',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  transition: 'all 0.3s ease',
+};
+
+
+// créer un objet de contexte
+
+// créer un composant Provider
+
