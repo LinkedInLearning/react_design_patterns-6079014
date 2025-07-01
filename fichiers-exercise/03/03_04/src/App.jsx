effffe532e1b119ccdeafe5735367cab5536e3c0
Sandy <sandy@MacBookPro.lan>
2025-07-01 12:36:45 +0200

    03_04

--- fichiers-exercise/03/03_04/src/App.jsx
+++ fichiers-exercise/03/03_04/src/App.jsx
@@ -1,27 +1,5 @@
 import React from 'react';
 
-const UserProfile = ({ user }) => {
-  return (
-    <div>
-      <h2>Profil de {user.name}</h2>
-      <p>Email : {user.email}</p>
-    </div>
-  );
-};
-
-
-const withLoading = (WrappedComponent) => {
-  return function WithLoadingComponent({ isLoading, ...props }) {
-    if (isLoading) {
-      return <div>Loading...</div>; // ou un spinner graphique
-    }
-
-    return <WrappedComponent {...props} />;
-  };
-};
-
-
-const UserProfileWithLoading = withLoading(UserProfile);
 
 const App = () => {
   const [user, setUser] = useState(null);
@@ -31,10 +9,13 @@ const App = () => {
     setTimeout(() => {
       setUser({ name: 'Alice', email: 'alice@example.com' });
       setLoading(false);
-    }, 2000); // simulation du chargement
+    }, 2000);
   }, []);
 
-  return <UserProfileWithLoading isLoading={loading} user={user} />;
+  return <div>
+    <h2>Profil de {user.name}</h2>
+    <p>Email : {user.email}</p>
+  </div>;
 };
 
 export default App;
