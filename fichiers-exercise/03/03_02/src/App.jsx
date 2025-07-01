b84c69b43181024d0cdc642b8947580d7915e67e
Sandy <sandy@MacBookPro.lan>
2025-07-01 11:34:17 +0200

    03_02_f

--- fichiers-exercise/03/03_02/src/App.jsx
+++ fichiers-exercise/03/03_02/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 // Composant
-const HelloComponent = ({ message }) => {
+const Heading1Component = ({ message }) => {
   return <h1>{message}</h1>;
+};
+
+const Heading2Component = ({ message }) => {
+  return <h2>{message}</h2>;
 };
 
 
@@ -14,10 +18,16 @@ const withMessage = (WrappedComponent, message = "Hello from HOC!") => {
 };
 
 // Utilisation du HOC : Composant amélioré
-const EnhancedComponent = withMessage(HelloComponent, "Bonjour depuis le HOC !");
+const EnhancedComponent = withMessage(Heading1Component, "Comprendre les HOC");
+const EnhancedComponent2 = withMessage(Heading2Component, "Définition des HOC");
 
 const App = () => {
-  return <EnhancedComponent />;
+  return (
+    <div>
+      <EnhancedComponent />
+      <EnhancedComponent2 />
+    </div>
+  );
 };
 
 export default App;
