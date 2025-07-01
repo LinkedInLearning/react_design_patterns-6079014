2f3470fa2bc158a3e41197dcba42dc8d804d4382
Sandy <sandy@MacBookPro.lan>
2025-07-01 14:16:39 +0200

    04_02_d

---
+++ fichiers-exercise/04/04_02/1/src/App.jsx
@@ -0,0 +1,17 @@
+import { createContext, useContext, useState } from "react";
+
+// créer un objet de contexte
+
+
+// créer le composant Provider
+
+
+// partager les données du contexte
+
+
+
+function App() {
+  return null
+}
+
+export default App
