35d9ee5077bfef1dcc27e8f1d4c57929edb60876
Sandy <sandy@MacBookPro.lan>
2025-07-03 11:29:41 +0200

    04_07_f

--- fichiers-exercise/04/04_07/src/App.jsx
+++ fichiers-exercise/04/04_07/src/App.jsx
@@ -1,51 +1,10 @@
-import React, { createContext, useContext, useState, useMemo } from 'react';
-const style = {
-  backgroundColor: isDark ? '#222' : '#f9f9f9',
-  color: isDark ? '#fff' : '#000',
-  height: '100vh',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  transition: 'all 0.3s ease',
-};
-
-
-// créer un objet de contexte
-const ThemeContext = createContext();
-
-// créer un composant Provider
-const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState(false);
-
-  const toggleTheme = () => setTheme(prevTheme => !prevTheme);
-
-  const isDark = useMemo(() => {
-    const isThemeDark = theme ? 'light' : 'dark';
-    return {
-      backgroundColor: isThemeDark === 'dark' ? '#222' : '#f9f9f9',
-      color: isThemeDark === 'dark' ? '#fff' : '#000',
-      height: '100vh',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      transition: 'all 0.3s ease',
-    };
-  }, [theme])
-
-  return (
-    <ThemeContext value={{ isDark, toggleTheme }}>
-      {children}
-    </ThemeContext>
-  );
-}
+import { useContext } from 'react';
+import { ThemeContext } from './context.jsx';
 
 const App = () => {
-  const { isDark, toggleTheme } = useContext(ThemeContext);
-
+  const { themeStyle, theme: isDark, toggleTheme } = useContext(ThemeContext);
   return (
-    <div style={isDark}>
+    <div style={themeStyle}>
       <h1>Thème {isDark ? 'sombre' : 'clair'}</h1>
       <button onClick={toggleTheme}>
         Basculer vers le thème {isDark ? 'clair' : 'sombre'}
---
+++ fichiers-exercise/04/04_07/src/context.jsx
@@ -0,0 +1,34 @@
+
+import { createContext, useState, useMemo } from 'react';
+
+// créer un objet de contexte
+export const ThemeContext = createContext();
+
+// créer un composant Provider
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(false);
+
+  const toggleTheme = () => setTheme(prevTheme => !prevTheme);
+
+  const isDark = useMemo(() => {
+    const isThemeDark = theme ? 'light' : 'dark';
+    return {
+      backgroundColor: isThemeDark === 'dark' ? '#222' : '#f9f9f9',
+      color: isThemeDark === 'dark' ? '#fff' : '#000',
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      transition: 'all 0.3s ease',
+    };
+  }, [theme])
+
+  return (
+    <ThemeContext value={{ themeStyle: isDark, theme, toggleTheme }}>
+      {children}
+    </ThemeContext>
+  );
+}
+
+export default ThemeProvider;
