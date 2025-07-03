5639c3fc62e4594c6da9dc8dc01c11ed851c3b61
Sandy <sandy@MacBookPro.lan>
2025-07-03 13:46:35 +0200

    05_03_f

--- fichiers-exercise/05/05_03/src/App.jsx
+++ fichiers-exercise/05/05_03/src/App.jsx
@@ -1,16 +1,14 @@
-import { useTheme } from './context.jsx';
-
+import { useContext } from 'react';
+import { ThemeContext } from './context.jsx';
 
 const App = () => {
-  const { themeStyle, theme: isDark, toggleTheme } = useTheme();
+  const { themeStyle, theme: isDark, toggleTheme } = useTheme(ThemeContext);
   return (
-    <div style={{ width: '100%' }}>
-      <div style={themeStyle}>
-        <h1>Thème {isDark ? 'sombre' : 'clair'}</h1>
-        <button onClick={toggleTheme}>
-          Basculer vers le thème {isDark ? 'clair' : 'sombre'}
-        </button>
-      </div>
+    <div style={themeStyle}>
+      <h1>Thème {isDark ? 'sombre' : 'clair'}</h1>
+      <button onClick={toggleTheme}>
+        Basculer vers le thème {isDark ? 'clair' : 'sombre'}
+      </button>
     </div>
   );
 }
