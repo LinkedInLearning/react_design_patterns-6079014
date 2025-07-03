42b46fddd5a50afa09b200aa33fe6f95069d25ed
Sandy <sandy@MacBookPro.lan>
2025-07-03 13:43:33 +0200

    05_03_f

--- fichiers-exercise/05/05_03/src/context.jsx
+++ fichiers-exercise/05/05_03/src/context.jsx
@@ -32,4 +32,13 @@ const ThemeProvider = ({ children }) => {
   );
 }
 
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+  return context;
+}
+
+
 export default ThemeProvider;
