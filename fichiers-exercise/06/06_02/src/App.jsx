f9ebb1856e1e8408e31bc6f58b9b4f5005380f87
Sandy <sandy@MacBookPro.lan>
2025-07-04 11:05:59 +0200

    06_02_f

--- fichiers-exercise/06/06_02/src/App.jsx
+++ fichiers-exercise/06/06_02/src/App.jsx
@@ -1,26 +1,34 @@
 import React, { useState, useMemo } from 'react';
 
-const style = {
-  backgroundColor: isDark === "Dark" ? '#222' : '#f9f9f9',
-  color: isDark === "Dark" ? '#fff' : '#000',
-  height: '100vh',
-  width: '100vw',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  transition: 'all 0.3s ease',
-}
 
 const ThemeToggleApp = () => {
+  const [theme, setTheme] = useState(false);
+
+  const toggleTheme = () => setTheme(prevTheme => !prevTheme);
+
+  const isDark = useMemo(() => theme ? "Clair" : "Sombre", [theme]);
+
+  const themeStyles = useMemo(() => {
+    return {
+      backgroundColor: theme ? '#222' : '#f9f9f9',
+      color: theme ? '#fff' : '#000',
+      height: '100vh',
+      width: '100vw',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      transition: 'all 0.3s ease'
+    }
+  }, [theme])
 
   return (
-    // <div style={style}>
-    //   <h2>Thème actuel : {isDark}</h2>
-    //   <button onClick={toggleTheme}>
-    //     Basculer le thème {isDark === "Dark" ? "Clair" : "Sombre"}
-    //   </button>
-    // </div>
+    <div style={themeStyles}>
+      <h2>Thème actuel : {isDark}</h2>
+      <button onClick={toggleTheme}>
+        Basculer le thème {isDark === "Dark" ? "Clair" : "Sombre"}
+      </button>
+    </div>
   );
 };
 
